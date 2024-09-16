8986d56b9875ad621a52b83234331f4316ba1675
Сергей Субботин <zarodiny@yandex.ru>
2024-09-16 14:34:16 +1000

    feat: добавляет стили

--- js/module/nav.js
+++ js/module/nav.js
@@ -2,22 +2,22 @@ export const initNav = () => {
     const body = document.querySelector('body');
     // Меню
     const mobileMenu = document.querySelector('.mobile-menu');
-    const mobileMenuCatalog = document.querySelector('.mobile-menu-catalog');
     const menuButtons = document.querySelector('.btn-close-menu');
     const mobileMenuLinks = document.querySelectorAll('.mobile-menu a');
 
 
     menuButtons.addEventListener('click', e => {
         menuButtons.classList.toggle('active');
-        // body.classList.toggle('lock');
+        mobileMenu.classList.toggle('is-active');
+        body.classList.toggle('lock');
     });
 
 
     mobileMenuLinks.forEach(link => {
         link.addEventListener('click', (evt) => {
-            // menuButtons.classList.remove('active');
+            menuButtons.classList.remove('active');
             mobileMenu.classList.remove('is-active');
-            // body.classList.remove('lock');
+            body.classList.remove('lock');
         });
     });
 }
