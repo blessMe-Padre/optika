daed491bbfec508c2524fa7993076b72b51d0329
Сергей Субботин <zarodiny@yandex.ru>
2024-09-16 14:54:55 +1000

    feat: добавляет стили

--- js/module/popup.js
+++ js/module/popup.js
@@ -1,6 +1,5 @@
 export const initPopup = () => {
     const popupLinks = document.querySelectorAll('.popup-link');
-    console.log(popupLinks)
     const body = document.querySelector('body');
     const lockPadding = document.querySelectorAll(".lock-padding");
     let unlock = true;
@@ -10,14 +9,14 @@ export const initPopup = () => {
 
     if (popupLinks.length > 0) {
         for (let index = 0; index < popupLinks.length; index++) {
-            
+
             const popupLink = popupLinks[index];
             popupLink.addEventListener("click", function (e) {
                 const popupName = popupLink.getAttribute('href').replace('#', '');
                 const curentPopup = document.getElementById(popupName);
                 popupOpen(curentPopup);
                 e.preventDefault();
-                
+
             });
         }
     }
--- js/module/sliders.js
+++ js/module/sliders.js
@@ -1,23 +1,24 @@
 export const initSlider = () => {
     const item = document.querySelector('.main-item');
-        let swiper;
-        if (item) {
-            swiper = new Swiper(item, {
-                autoplay: {
-                    delay: 3000
-                },
-                speed: 3000,
-                effect: "fade",
-                direction: 'horizontal',  
-                spaceBetween: 15,
-                slidesPerView: 1,
-                equalHeight: true,
+    let swiper;
+    if (item) {
+        swiper = new Swiper(item, {
+            autoplay: {
+                delay: 3000
+            },
+            speed: 3000,
+            // effect: "fade",
+            direction: 'horizontal',
+            spaceBetween: 15,
+            slidesPerView: 1,
+            equalHeight: true,
 
-                // If we need pagination
-                pagination: {
-                    el: '.swiper-pagination',
-                    type: 'bullets',
-                  },
-              });
-        }
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets',
+                clickable: true
+            },
+        });
+    }
 }
